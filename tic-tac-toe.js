7fbbeecc17af03b6571cebe68fd257ba39140d50
zanovia Butler <joshuameghoo@gmail.com>
2023-10-22 21:26:19 -0600

    Added functionality to place X or O on square click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all the div elements inside the #board element
     const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';  // Start with player X
+    let boardState = Array(9).fill(' ');  // Initial state of the board
 
-    // Add the 'square' class to each div element
-    squares.forEach(square => {
+    squares.forEach((square, index) => {
         square.classList.add('square');
+
+        // Attach click event listener to each square
+        square.addEventListener('click', function() {
+            // Check if the square is empty and if so, make the move
+            if (boardState[index] === ' ') {
+                boardState[index] = currentPlayer;
+
+                square.textContent = currentPlayer; // Set the text of the square to X or O
+                square.classList.add(currentPlayer); // Add the X or O class for styling
+
+                // Toggle the current player
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
     });
 });
